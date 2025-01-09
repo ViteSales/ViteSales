fff9e1d7e99d07cc046234b61618b3b9f608ff0c
Mohammad Julfikar <jxmxshakil@gmail.com>
2025-01-09 20:20:18 +0800

    Add new Auth and Deployment modules with required services
    Introduced `ViteSales.ERP.Auth` and `ViteSales.ERP.Deployment` projects targeting .NET 9.0. The Auth module integrates authentication and caching services, while the Deployment module includes a DigitalOcean API client setup. Both modules are foundational for extending ERP functionality.

---
+++ ViteSales.ERP.Auth/ViteSalesAuth.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ViteSales.ERP.Auth.Client;
+using ViteSales.ERP.Auth.Interfaces;
+using ViteSales.ERP.Auth.Services;
+using ViteSales.Shared.Cache;
+using ViteSales.Shared.Interfaces;
+using ViteSales.Shared.Models;
+using ViteSales.Shared.Utils;
+
+namespace ViteSales.ERP.Auth;
+
+public class ViteSalesAuth
+{
+    private readonly ServiceCollection _services = new();
+    public ViteSalesAuth(AppSettings appSettings)
+    {
+        var cacheCredentials = appSettings.CacheDb;
+        var authSecrets = appSettings.AuthSecrets;
+        var client = new AccessToken(authSecrets);
+        var cache = new CacheClient(new CacheDbCredentials
+        {
+            Host = cacheCredentials.Host,
+            User = cacheCredentials.User,
+            Password = cacheCredentials.Password,
+            Port = cacheCredentials.Port,
+            Database = cacheCredentials.Database
+        },"ViteSalesAuth");
+        cache.Connect().Wait();
+        
+        _services.Configure<AuthSecrets>(options =>
+        {
+            options.AuthorityUrl = authSecrets.AuthorityUrl;
+            options.ClientId = authSecrets.ClientId;
+            options.ClientSecret = authSecrets.ClientSecret;
+            options.Audience = authSecrets.Audience;
+            options.GrantType = authSecrets.GrantType;
+            options.AuthDomain = authSecrets.AuthDomain;
+        });
+        _services.AddSingleton<ICacheClient>(cache);
+        _services.AddSingleton<IAccessToken>(client);
+        _services.AddTransient<IAuthentication, AuthenticationService>();
+        _services.AddTransient<IOrganization, OrganizationService>();
+        _services.AddTransient<IUser, UserService>();
+    }
+    
+    public ServiceCollection GetServiceCollection()
+    {
+        return _services;
+    }
+
+    public ServiceProvider Build()
+    {
+        return _services.BuildServiceProvider();
+    }
+}
