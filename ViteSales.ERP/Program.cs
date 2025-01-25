f1bf4b0311dbb395951d47b00102e48389cdf0c1
Mohammad Julfikar <jxmxshakil@gmail.com>
2025-01-25 09:58:20 +0800

    project added

---
+++ ViteSales.ERP/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using ViteSales.ERP.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddSingleton<PowerPointService>();
+            builder.Services.AddSingleton<WordService>();
+            builder.Services.AddSingleton<PdfService>();
+            builder.Services.AddSingleton<ExcelService>();
+builder.Services.AddRazorPages();
+            builder.Services.AddSignalR(e => { e.MaximumReceiveMessageSize = 102400000; });
+            builder.Services.AddSignalR(e => { e.MaximumReceiveMessageSize = 102400000; });
+builder.Services.AddServerSideBlazor().AddHubOptions(o=>
+            {
+                o.MaximumReceiveMessageSize=102400000;
+            });
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
